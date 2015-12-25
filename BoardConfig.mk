4d741c17492ee0535bb7a6d88353e43a388659cb
spezi77 <spezi77@gmx.com>
2015-12-25 22:21:12 +0100

    Update kernel path
    Change-Id: Idfbe67d6ba94f660088dfe3697799f15135c12a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Inline kernel building
 TARGET_KERNEL_CONFIG := mako_defconfig
-TARGET_KERNEL_SOURCE := kernel/lge/mako
+TARGET_KERNEL_SOURCE := kernel/google/msm
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
 
