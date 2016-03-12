ccc8986e3e840c00bf63b113feea4c4594473862
spezi77 <spezi77@gmx.com>
2016-03-12 19:08:56 +0100

    MAKO: unbreak videorecording with Snap
    Change-Id: I69a8f52b508690d56b035feab63ab60bd7fe1090
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- device.mk
+++ device.mk
@@ -274,5 +274,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
 	Snap
 
+# unbreak videorecording with Snap
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.camera.cpp.duplication=false
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
